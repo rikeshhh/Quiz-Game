e39c3db2646cbc538ec49f8afed6213214eeac27
rikesh <70895896+rikeshhh@users.noreply.github.com>
2023-12-14 16:39:48 +0545

    simple quiz app consisting four questions and displays error and total user corret answers as well with green color means correct and red defines error

--- index.js
+++ index.js
@@ -1,45 +1,74 @@
 const questions = [
-    {
-      question: "what is my name?",
-      answer: [
-        { text: "rikesh", correct: true },
-        { text: "nishan", correct: false },
-        { text: "bimal", correct: false },
-      ],
-    },
-    {
-      question: "what is the color of the sky??",
-      answer: [
-        { text: "blue", correct: true },
-        { text: "orange", correct: false },
-        { text: "pink", correct: false },
-      ],
-    },
-  ];
-  
-  const questionElement = document.getElementById("question__container");
-  
-  for (let index = 0; index < questions.length; index++) {
-    let header = document.createElement("h1");
-    header.textContent = questions[index].question;
-    questionElement.appendChild(header);
-  
-    for (let j = 0; j < questions[index].answer.length; j++) {
-      let button = document.createElement("button");
-      button.classList.add("btn");
-      button.textContent = questions[index].answer[j].text;
-      questionElement.appendChild(button);
-  
-      // Pass a reference to the checkAnswer function, not the result of its invocation
-      button.addEventListener("click", () => checkAnswer(questions[index].answer[j]));
-    }
+  {
+    question: "Which country drinks the most amount of coffee per person?",
+    answer: [
+      { text: "Finland", correct: true },
+      { text: "Italy", correct: false },
+      { text: "Colombo", correct: false },
+    ],
+  },
+  {
+    question: "What color is an airplane’s famous black box?",
+    answer: [
+      { text: "blue", correct: false },
+      { text: "orange", correct: true },
+      { text: "pink", correct: false },
+    ],
+  },
+  {
+    question: "Who is considered the founder of modern computer science?",
+    answer: [
+      { text: "Alan turing", correct: true },
+      { text: "Steve Jobs", correct: false },
+      { text: "Bill Gates", correct: false },
+    ],
+  },
+  {
+    question: "in Ancient Rome, how many days of the week were there?",
+    answer: [
+      { text: "Two", correct: false },
+      { text: "Four", correct: false },
+      { text: "Eight", correct: true },
+    ],
+  },
+];
+let total = 0;
+let error = 0;
+const score = document.getElementById("score");
+const mistake = document.getElementById("mistake")
+const questionElement = document.getElementById("question__container");
+
+for (let index = 0; index < questions.length; index++) {
+  let header = document.createElement("h1");
+  header.textContent = questions[index].question;
+  const Resetbutton = document.createElement('button')
+  Resetbutton.innerText = "Reset",
+  questionElement.appendChild(header);
+
+  for (let j = 0; j < questions[index].answer.length; j++) {
+    let button = document.createElement("button");
+    button.classList.add("btn");
+    button.textContent = questions[index].answer[j].text;
+    questionElement.appendChild(button);
+
+    button.addEventListener("click", () =>
+      checkAnswer(button,questions[index].answer[j])
+    );
   }
-  
-  function checkAnswer(answer) {
-    if (answer.correct === true) {
-      alert("correct");
-    } else {
-      alert("incorrect");
-    }
+
+}
+
+function checkAnswer(button,answer) {
+  if (answer.correct === true) {
+    total = total + 1;
+    score.textContent = `your total score:  ${total}`;
+  alert('correct')
+  button.classList.add("correct")
+  } else {
+    alert("incorrect");
+    error = error +1;
+mistake.textContent =  `Mistake: ${error}`
+    button.classList.add('incorrect')
   }
-  
+
+}
